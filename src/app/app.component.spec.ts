7794118f0eb17c9bb1d639b5ae1dc7a79bc4156c
aymen <aymen.sellaouti@gmail.com>
2019-08-21 12:24:00 +0100

    init commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'bd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bd');
+    expect(app.title).toEqual('aymen');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bd!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to aymen');
   });
 });
